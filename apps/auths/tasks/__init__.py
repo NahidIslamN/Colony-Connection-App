636a98292aff54fb804a3783f82dc310c57a2ff3
Nahid Islam <nahidislam740405@gmail.com>
2026-04-25 10:24:09 +0600

    remove update_last_activity task; clean up imports and __all__ declaration in tasks module

--- apps/auths/tasks/__init__.py
+++ apps/auths/tasks/__init__.py
@@ -1,4 +1,3 @@
-from apps.auths.tasks.activity_tasks import update_last_activity
 from apps.auths.tasks.email_tasks import send_email_to
 
-__all__ = ["send_email_to", "update_last_activity"]
+__all__ = ["send_email_to"]
